c5bf8a6b9ad2e151ad580d35c4b5a089e557dc5a
PabloSantos-2023251 <psantos-2023251@kinal.edu.gt>
2026-06-17 11:02:03 -0600

    refractor: se cambio la interfaz de Beneficiario para que utilice types

--- ProyectoBIM3/src/models/Beneficiario.ts
+++ ProyectoBIM3/src/models/Beneficiario.ts
@@ -1,8 +1,10 @@
-interface Beneficiario {
+import { DepartamentoGT } from './Departamentos';
+
+export interface Beneficiario {
     id_beneficiario?: number;
     cui_dpi: string;
     nombre_completo: string;
     direccion_comunidad: string;
-    departamento: string;
+    departamento: DepartamentoGT;
     cantidad_dependientes?: number;
 }
