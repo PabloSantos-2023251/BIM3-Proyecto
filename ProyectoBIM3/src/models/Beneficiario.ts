8aa9b4385ec7787b636fe032e00331124e4ea9e5
PabloSantos-2023251 <psantos-2023251@kinal.edu.gt>
2026-06-17 10:35:30 -0600

    feat: se agrego la interfaz de beneficiario

---
+++ ProyectoBIM3/src/models/Beneficiario.ts
@@ -0,0 +1,8 @@
+interface Beneficiario {
+    id_beneficiario?: number;
+    cui_dpi: string;
+    nombre_completo: string;
+    direccion_comunidad: string;
+    departamento: string;
+    cantidad_dependientes?: number;
+}
