3d3714115377a31abdf5f5b4f846b98e58ba0284
PabloSantos-2023251 <psantos-2023251@kinal.edu.gt>
2026-06-17 10:58:17 -0600

    feat: se creo el typr de los departamentos de guatemala

---
+++ ProyectoBIM3/src/models/Departamentos.ts
@@ -0,0 +1,7 @@
+export type DepartamentoGT = 
+    | 'Alta Verapaz' | 'Baja Verapaz' | 'Chimaltenango' | 'Chiquimula' 
+    | 'El Progreso' | 'Escuintla' | 'Guatemala' | 'Huehuetenango' 
+    | 'Izabal' | 'Jalapa' | 'Jutiapa' | 'Petén' 
+    | 'Quetzaltenango' | 'Quiché' | 'Retalhuleu' | 'Sacatepéquez' 
+    | 'San Marcos' | 'Santa Rosa' | 'Sololá' | 'Suchitepéquez' 
+    | 'Totonicapán' | 'Zacapa';
